56fd5d164df98b3684e4135c3cbfbca2f1188a6e
Thao Huynh <huynhquocthao@gmail.com>
2018-02-19 18:06:41 +0700

    Add authentication FireBase

--- scenes/fireBaseServices.js
+++ scenes/fireBaseServices.js
@@ -12,4 +12,42 @@ const config = {
 firebase.initializeApp(config);
 const FBDatabase = firebase.database();
 
+/**
+ * FireBase Authorization
+ * signInWithEmailAndPassword: email, password => in CONSOLE APP (Authentication tab)
+ * onAuthStateChanged
+ */
+
+const authFireBase = () => {
+	const user = {
+		email: 'huynhquocthao@gmail.com',
+		password: '9502121',
+	};
+
+	firebase.auth().signInWithEmailAndPassword(user.email, user.password).catch(error => {
+		// Handle Errors here.
+		console.log('LOGIN FAIL', error);
+	});
+
+	firebase.auth().onAuthStateChanged(user => {
+		if (user) {
+			// User is signed in.
+			console.log('SIGNED IN', user.email);
+			// const displayName = user.displayName;
+			// const email = user.email;
+			// const emailVerified = user.emailVerified;
+			// const photoURL = user.photoURL;
+			// const isAnonymous = user.isAnonymous;
+			// const uid = user.uid;
+			// const providerData = user.providerData;
+		} else {
+			// User is signed out.
+			console.log('SIGNED OUT');
+		}
+	});
+};
+
+authFireBase();
+
 export default FBDatabase;
+
